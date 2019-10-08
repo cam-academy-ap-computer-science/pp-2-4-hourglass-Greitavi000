ce8eb1982cf57acb7fae15adbdd0b962349b214c
greitavi000 <greitavi000@CAM-LAB-20.bgsd.k12.wa.us>
2019-10-08 07:53:05 -0700

    HourglassDay1.java

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -3,5 +3,13 @@
 */
 
 public class Hourglass {
-
+	public static final int Constant = 5;
+	{
+	for(int g = 1; g<=4; g++ ) {
+		System.out.print(" ");
+	}
+    for(int j = 8; j>= 1 ; j-- ) {
+    	System.out.print(":");
+	}
+}
 }
