e8727541d882227d77cf688d35ce8a4eadb7e7f3
greitavi000 <greitavi000@CAM-LAB-20.bgsd.k12.wa.us>
2019-10-08 07:53:42 -0700

    HourglassDay1.java

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -3,13 +3,17 @@
 */
 
 public class Hourglass {
-	public static final int Constant = 5;
+	public static final int AAAA = 1;
 	{
-	for(int g = 1; g<=4; g++ ) {
+	for(int g = AAAA; g<=4; g++ ) { //* for the all top \:/.  *//
 		System.out.print(" ");
-	}
-    for(int j = 8; j>= 1 ; j-- ) {
+		for(int h = AAAA; h<=4; h++) { //* for the spases.  *//
+			System.out.print(" ");
+		}
+     for(int j = (-2*g)+10; j>= 2 ; j-- ) {
     	System.out.print(":");
+    	
 	}
 }
+	}
 }
